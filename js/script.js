58e4f22178ec2aebba300e62fb9e469f07a6ca39
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-07-23 15:09:24 +0200

    Flyttade gamla versionen till Old version

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.querySelector('.toggle-button');
-const navbarMenu = document.querySelectorAll('.container-3');
-console.log(toggleButton, navbarMenu);
-
-toggleButton.addEventListener('click', () => {
-    navbarMenu.forEach((n) => {n.classList.toggle('active')})
-});
