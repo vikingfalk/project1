4bc08964567ddae26b66bde3f3b55e2310b7a244
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-07-28 12:35:06 +0200

    Ganska bra mobilversion nu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 const toggleButton = document.querySelector('.toggle-button');
-const navbarMenu = document.querySelectorAll('.container-3');
-console.log(toggleButton, navbarMenu);
+const navbarLinks = document.querySelector('.navbar-links');
+const navbar = document.querySelector('.navbar');
+const searchButton = document.querySelector('#search-button');
+const searchCancel = document.querySelector('#search-cancel');
+const navbarSearch = document.querySelector('.mobile-nav-search');
 
 toggleButton.addEventListener('click', () => {
-    navbarMenu.forEach((n) => {n.classList.toggle('active')})
+    navbarLinks.classList.toggle('active')
+});
+
+searchButton.addEventListener('click', () => {
+    navbarSearch.classList.toggle('active');
+    document.querySelector('.mobile-nav-search input').focus();
+});
+
+searchButton.addEventListener('click', () => {
+    navbarSearch.classList.toggle('transition');
+});
+
+searchCancel.addEventListener('click', () => {
+    navbarSearch.classList.remove('active');
+    navbarSearch.classList.remove('transition');
+});
+
+document.addEventListener('click', (event) => {
+    if (!navbar.contains(event.target)) {
+        navbarLinks.classList.remove('active')
+    }
 });
