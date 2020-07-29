962c4c31b91790264e559f53aef585cff9c30c5f
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-07-29 13:48:34 +0200

    .

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ const navAccount = document.querySelector('#nav-account');
 const loginMenu = document.querySelector('.login-menu');
 const cancelButton = document.querySelector('.cancel-button');
 const html = document.querySelector('html');
-const body = document.querySelector('body');
 const viewportHeight = html.clientHeight;
+const background = document.querySelector('#background');
 const user = navigator.userAgent.toLowerCase();
 const isAndroid = user.indexOf("android") > -1 && user.indexOf("mobile") > -1;
 
 
-// //fixa bakgrunden för android
-// if (isAndroid) {
-//     document.querySelector('body').style.backgroundSize = "auto " + (viewportHeight + 60) + "px";
-// }
+//fixa bakgrunden för android
+if (isAndroid) {
+    background.style.minHeight = getComputedStyle(background).height;
+}
 
 
 //mobilvy kontoknapp
