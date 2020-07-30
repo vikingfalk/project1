50fb2298b0bdbf5e78995bb2509091f3815b98b7
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-07-30 15:45:42 +0200

    Klar för att visas typ

--- js/script.js
+++ js/script.js
@@ -7,6 +7,37 @@ const navbarSearch = document.querySelector('.mobile-nav-search');
 const navAccount = document.querySelector('.nav-account');
 const loginMenu = document.querySelector('.login-menu');
 const cancelButton = document.querySelector('.cancel-button');
+const catBtn = document.querySelector('#categories-button');
+const brands = document.querySelectorAll('.brand');
+
+//ajax
+fetch('../html/landing.txt')
+    .then((res) => res.text())
+    .then((data) => {
+        document.querySelector('#content').innerHTML = data;
+    });
+
+catBtn.addEventListener('click', () => {
+    fetch('../html/categories.txt')
+        .then((res) => res.text())
+        .then((data) => {
+            document.querySelector('#content').innerHTML = data;
+        });
+
+    navbarLinks.classList.remove('active');
+});
+
+brands.forEach((brand) => {
+    brand.addEventListener('click', () => {
+        fetch('../html/landing.txt')
+            .then((res) => res.text())
+            .then((data) => {
+                document.querySelector('#content').innerHTML = data;
+            });
+
+        navbarLinks.classList.remove('active');
+    });
+});
 
 
 //kontoknapp
