2c7fb682274f7e447bbf5c642b2c07e6646bcdb6
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-07-30 14:14:37 +0200

    ganska najs landningssida

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 const navbar = document.querySelector('.navbar');
-const searchButton = document.querySelector('#search-button');
-const searchCancel = document.querySelector('#search-cancel');
+const searchButton = document.querySelectorAll('.search-button');
+const searchCancel = document.querySelector('.search-cancel');
 const navbarSearch = document.querySelector('.mobile-nav-search');
-const navAccount = document.querySelector('#nav-account');
+const navAccount = document.querySelector('.nav-account');
 const loginMenu = document.querySelector('.login-menu');
 const cancelButton = document.querySelector('.cancel-button');
-const html = document.querySelector('html');
-const viewportHeight = html.clientHeight;
-const background = document.querySelector('#background');
-const user = navigator.userAgent.toLowerCase();
-const isAndroid = user.indexOf("android") > -1 && user.indexOf("mobile") > -1;
 
 
-//fixa bakgrunden för android
-if (isAndroid) {
-    background.style.minHeight = getComputedStyle(background).height;
-}
-
-
-//mobilvy kontoknapp
+//kontoknapp
 navAccount.addEventListener('click', () => {
-    loginMenu.classList.add('active')
-    navbarLinks.classList.remove('active')
+    loginMenu.classList.add('active');
+
+    navbarLinks.classList.remove('active');
 });
 
 cancelButton.addEventListener('click', () => {
@@ -33,7 +23,10 @@ cancelButton.addEventListener('click', () => {
 
 //mobilmenyknappen
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
+
+    navbarSearch.classList.remove('active');
+    navbarSearch.classList.remove('transition');
 });
 
 document.addEventListener('click', (event) => {
@@ -44,14 +37,19 @@ document.addEventListener('click', (event) => {
 
 
 //nav söken
-searchButton.addEventListener('click', () => {
-    navbarSearch.classList.toggle('active');
-    document.querySelector('.mobile-nav-search input').focus();
+searchButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        navbarSearch.classList.toggle('active');
+        document.querySelector('.mobile-nav-search input').focus();
+    });
 });
 
-searchButton.addEventListener('click', () => {
-    navbarSearch.classList.toggle('transition');
-    navbarLinks.classList.remove('active')
+searchButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        navbarSearch.classList.toggle('transition');
+
+        navbarLinks.classList.remove('active');
+    });
 });
 
 searchCancel.addEventListener('click', () => {
